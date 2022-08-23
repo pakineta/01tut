48b61a205b56132d69638033d47048b8eb62c30d
unknown <nicolas.cuatrini@gmail.com>
2022-08-23 12:29:37 -0300

    add deleteOptions in handleDelete function

--- src/App.js
+++ src/App.js
@@ -68,9 +68,14 @@ function App() {
 
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = {method: 'DELETE'}
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if(result) setFetchError(result);
   }
 
   const handleSubmit = (e) => {
